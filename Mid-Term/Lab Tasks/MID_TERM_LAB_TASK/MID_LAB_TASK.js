23ddad5573924b9ff8030f160fbfc65001507856
Arnob-HRes <arnobsarkersupta.education@gmail.com>
2025-11-17 11:44:40 +0600

    MID-LAB TASK JS 2

--- Mid-Term/Lab Tasks/MID_TERM_LAB_TASK/MID_LAB_TASK.js
+++ Mid-Term/Lab Tasks/MID_TERM_LAB_TASK/MID_LAB_TASK.js
@@ -4,36 +4,36 @@ function Reg(){
     let PhoneNumber=document.getElementById("I3").value.trim();
     let Password=document.getElementById("I4").value.trim();
     let ConfirmPassword=document.getElementById("I5").value.trim();
-    let DIV=document.getElementById("D1");
+    let DIV1=document.getElementById("D1");
+    let DIV2=document.getElementById("D1");
 
     if(FullName === "" || Email === "" || PhoneNumber === "" || Password === "" || ConfirmPassword === ""){
-        DIV.style.backgroundColor = "indianred";
-        DIV.style.color = "white";
-        DIV.innerHTML = "Everything must not be empty";
+        DIV1.style.backgroundColor = "indianred";
+        DIV1.style.color = "white";
+        DIV1.innerHTML = "Everything must not be empty";
     }
     
     let ValidEmail = Email.includes("@");
 
     if(ValidEmail === false){
-        DIV.style.backgroundColor = "indianred";
-        DIV.style.color = "white";
-        DIV.innerHTML = "Invalide Email! Must containt '@'";
+        DIV1.style.backgroundColor = "indianred";
+        DIV1.style.color = "white";
+        DIV1.innerHTML = "Invalide Email! Must containt '@'";
     }
 
     if(isNaN(PhoneNumber)){
-        DIV.style.backgroundColor = "indianred";
-        DIV.style.color = "white";
-        DIV.innerHTML = "Phone Number Must be Numbers";
-        return false;
+        DIV1.style.backgroundColor = "indianred";
+        DIV1.style.color = "white";
+        DIV1.innerHTML = "Phone Number Must be Numbers";
     }
 
     if(Password != ConfirmPassword){
-        DIV.style.backgroundColor = "indianred";
-        DIV.style.color = "white";
-        DIV.innerHTML = "Password and Confirm Password must match";
+        DIV1.style.backgroundColor = "indianred";
+        DIV1.style.color = "white";
+        DIV1.innerHTML = "Password and Confirm Password must match";
     }
 
-    DIV.innerHTML=`
+    DIV2.innerHTML=`
     Registration Successful <br>
     Name: ${FullName} <br>
     Email: ${Email} <br>
