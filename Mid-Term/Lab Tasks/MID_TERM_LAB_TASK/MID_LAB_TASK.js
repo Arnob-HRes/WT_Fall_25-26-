9ec2899a2a1aade11068e0c4bad6769b1e8e7e6d
Arnob-HRes <arnobsarkersupta.education@gmail.com>
2025-11-17 11:31:46 +0600

    MID-LAB TASK JS 1

---
+++ Mid-Term/Lab Tasks/MID_TERM_LAB_TASK/MID_LAB_TASK.js
@@ -0,0 +1,21 @@
+function Reg(){
+    let FullName=document.getElementById("I1").value;
+    let Email=document.getElementById("I2").value.trim();
+    let PhoneNumber=document.getElementById("I3").value.trim();
+    let Password=document.getElementById("I4").value.trim();
+    let ConfirmPassword=document.getElementById("I5").value.trim();
+    let DIV=document.getElementById("D1");
+
+    if(FullName === "" || Email === "" || PhoneNumber === "" || Password === "" || ConfirmPassword === ""){
+        DIV.style.backgroundColor = "indianred";
+        DIV.style.color = "white";
+        DIV.innerHTML = "Everything must not be empty";
+    }
+    
+    let ValidEmail = Email.includes("@");
+    if(ValidEmail === false){
+        DIV.style.backgroundColor = "indianred";
+        DIV.style.color = "white";
+        DIV.innerHTML = "Invalide Email! Must containt '@'";
+    }
+}
