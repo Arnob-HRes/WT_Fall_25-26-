0f7e47bf34ef1a4d4729b16335c53cc43e87b0ef
Arnob-HRes <arnobsarkersupta.education@gmail.com>
2025-12-22 12:17:15 +0600

    WT FINAL Lab 3

--- Final-Term/Lab Tasks/Lab Task 3/Login.php
+++ Final-Term/Lab Tasks/Lab Task 3/Login.php
@@ -1,47 +1,48 @@
 <?php
 session_start();
-
-if(isset($_SESSION["user"])){
+if (isset($_SESSION["username"]))
+{
     header("Location:dashboard.php");
     exit();
 }
+if($_SERVER["REQUEST_METHOD"]=="POST")
+{
+$user=$_POST["username"];
+$pass=$_POST["password"];
 
-if($_SERVER["REQUEST_METHOD"]=="POST"){
-    $name=$_POST["user"];
-    $pass=$_POST["pass"];
+if ($user=="admin" && $pass=="1234")
+{
 
-    if($user=="admin" && $pass=="1234"){
-        $_SESSION["user"]=$user;
-        header("Location:dashboard.php");
-        exit();
-    }
-    else{
-        $error="Invalide username or password";
-    }
+    $_SESSION["username"] = $user;
+    header("Location:dashboard.php");
+    exit();
 }
+else{
+
+    $error="Invalid Id and pass";
+}
+
+}
+
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>
-            LOGIN
-        </title>
-    </head>
-    <body>
-    <form method="post">
-        Username:
-        <center>
-            <input type="text" name="user" value="<?php echo $user;?>">
-            <?php echo $nameerror; ?>
-        </center>
-        Password:
-        <center>
-            <input type="text" name="pass" value="<?php echo $pass;?>">
-            <?php echo $passerror; ?>
-        </center>
-        <center>
-            <input type="submit" name="login" value="LOGIN">
-        </center>
-    </form>
-    </body>
+<head>
+    <title>Login</title>
+</head>
+<body>
+
+<h2>Login</h2>
+
+<form method="post">
+    <input type="text" name="username" placeholder="Username"><br><br>
+    <input type="password" name="password" placeholder="Password"><br><br>
+    <input type="submit" value="Login">
+</form>
+
+<p style="color:red;">
+    <?php if (isset($error)) echo $error; ?>
+</p>
+
+</body>
 </html>
