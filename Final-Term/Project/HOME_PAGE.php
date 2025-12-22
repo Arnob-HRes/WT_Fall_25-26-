76c71defdab1f00b872e81da3038fcc394947c7c
Arnob-HRes <arnobsarkersupta.education@gmail.com>
2025-12-23 00:38:50 +0600

    WT Project LOGIN-PAGE 1

---
+++ Final-Term/Project/HOME_PAGE.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Local Festival Stall Allocation & Fee Management System
+        </title>
+    </head>
+    <style>
+        #F1{
+            position: absolute;
+            top: 280px;
+            right: 150px;
+            width: 500px;
+            height: auto;
+            text-align: center;
+        }
+        #B1{
+            width: 300px;
+            height: 50px;
+            text-align: center;
+            color: white;
+            background-color: blue;
+            border: 1px solid blue;
+            border-radius: 10px;
+            margin:20px;
+        }
+        #B2{
+            width: 300px;
+            height: 50px;
+            text-align: center;
+            color: blue;
+            background-color: lightblue;
+            border: 1px solid lightblue;
+            border-radius: 10px;
+            margin:20px;
+        }
+        #D1{
+            position: absolute;
+            top:50px;
+            right: 250px;
+            background-color: white;
+            width: auto;
+            height: auto;
+        }
+        #D2{
+            position: absolute;
+            top:120px;
+            right: 310px;
+            background-color: white;
+            width: auto;
+            height: auto;
+        }
+        #D3{
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            background-color: blue;
+            border-radius: 20px;
+            margin: 5px;
+            width: 700px;
+            height: 720px;
+        }
+    </style>
+    <body>
+        <div id="D3"></div>
+        <div id="D1">
+            <h1 style="font-size: 40px;">Local Festival Stall</h1>
+        </div>
+        <div id="D2">
+            <h1 style="font-size: 40px;">Allocation</h1>
+        </div>
+        <form id="F1">
+            <hr>
+            <button id="B1">
+                LOGIN
+            </button>
+            <button id="B2">
+                SIN UP
+            </button> <br>
+            <hr>
+        </form>
+    </body>
+</html>
