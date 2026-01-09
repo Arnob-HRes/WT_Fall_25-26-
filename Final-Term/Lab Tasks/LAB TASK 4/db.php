2d20a1b07ec4fc40653cd2650d67c2a53aba781f
Arnob-HRes <arnobsarkersupta.education@gmail.com>
2026-01-09 21:04:00 +0600

    sdds

--- Final-Term/Lab Tasks/LAB TASK 4/db.php
+++ Final-Term/Lab Tasks/LAB TASK 4/db.php
@@ -7,7 +7,7 @@ $dbname="WT_O";
 $conn= new mysqli($host,$user,$pass,$dbname);
 
 if($conn->connect_error){
-    die("Connection Fail: ". $conn->connect_error);//
+    die("Connection Fail: ". $conn->connect_error);
 }
 
 ?>
